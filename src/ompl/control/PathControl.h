237c7121dc088b9c5a94412fb6fb047cc091a32d
Ioan Sucan <isucan@willowgarage.com>
2013-05-31 15:54:02 +0300

    Rebase from 'debian/hydro/ompl'

--- src/ompl/control/PathControl.h
+++ src/ompl/control/PathControl.h
@@ -79,6 +79,15 @@ namespace ompl
 
             /** \brief Print the path to a stream */
             virtual void print(std::ostream &out) const;
+            /** \brief Print the path as a real-valued matrix where the
+                i-th row represents the i-th state along the path, followed
+                by the control and duration needed to reach this state. For
+                the first state the control and duration are zeroes. The
+                state components printed are those returned by
+                ompl::base::StateSpace::copyToReals, while the control
+                components printed are those returned by
+                ompl::control::ControlSpace::getValueAddressAtIndex. */
+            virtual void printAsMatrix(std::ostream &out) const;
 
             /** \brief Convert this path into a geometric path (interpolation is performed and then states are copied) */
             geometric::PathGeometric asGeometric(void) const;
@@ -86,7 +95,7 @@ namespace ompl
             /** @name Path operations
                 @{ */
 
-            /** \brief Append \e state to the end of the path; it is assumed \e state is the last state, so no control is applied.
+            /** \brief Append \e state to the end of the path; it is assumed \e state is the first state, so no control is applied.
                 The memory for \e state is copied. There are no checks to make sure the number of controls and states make sense. */
             void append(const base::State *state);
 
